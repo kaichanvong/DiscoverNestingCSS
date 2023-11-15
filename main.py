753a927bc4d6ba26dbe60be1b63884bcb43479d9
kaichanvong <kai.vong@gmail.com>
2023-11-15 17:01:49 +0000

    edit example into script-runner

--- main.py
+++ main.py
@@ -7,31 +7,14 @@ CLOSE_COMMENT = '*' + '/'
 
 
 class NestingCascadingStyleSheets:
-    def __init__(self):
+    def __init__(self, styles):
+        self.css_examples = styles
         self.nesting_cascading_style_sheets()
 
     def parenthesis_objects(self):
         pass
 
     def nesting_cascading_style_sheets(self):
-        css_examples = ["""
-                    .card {
-                  h1 {
-                    /* this is now valid! */
-                    font-weight: 200;
-                    font-family: helvetica;
-                    display: block;
-                    min-width: 200px;
-                    min-height: 1em;
-                  }
-                }""", """
-                /* the same as */
-                .card {
-                  & h1 {
-                    /* this is now valid! */
-                  }
-                }
-                """]
         multi_chars = alphas + '.' + '1'
         comments = OPEN_COMMENT + CLOSE_COMMENT
         parenthesis_switch = PARENTHESIS[0] + PARENTHESIS[1]
@@ -44,9 +27,30 @@ class NestingCascadingStyleSheets:
         nesting6[1] = (Word(multi_chars) + parenthesis_mix + Word(multi_chars))
         nesting6[1] += (parenthesis_mix + properties_plus_comments)
         nesting6[1] += (parenthesis_mix + parenthesis_mix)
-        print(nesting6[1].parseString(css_examples[0]))
+        css_example = self.css_examples[0]
+        print(nesting6[1].parseString(css_example))
 
 
 if __name__ == '__main__':
-    nest_this = NestingCascadingStyleSheets()
-    print(nest_this)
+    css_examples = ["""
+                .card {
+              h1 {
+                /* this is now valid! */
+                font-weight: 200;
+                font-family: helvetica;
+                display: block;
+                min-width: 200px;
+                min-height: 1em;
+              }
+            }""", """
+            /* the same as */
+            .card {
+              & h1 {
+                /* this is now valid! */
+              }
+            }
+            """]  # discovered @ link
+    #  link: https://developer.chrome.com/blog/css-nesting-relaxed-syntax-update
+    # anchor name /#nesting-element-tag-names
+    NestingCascadingStyleSheets(css_examples)
+
